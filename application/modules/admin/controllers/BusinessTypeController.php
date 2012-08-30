8f83981d6105e1c0af26a4dddea350a53f99f406
Bad Hack <thekingofall@rocketmail.com>
2012-08-30 10:01:27 +0530

    BlockUI & Some CSS changes
    Blockui added, grid data loaded according to jQuery queue and .htaccess
    changed for director listing. Also images for datatables added

--- application/modules/admin/controllers/BusinessTypeController.php
+++ application/modules/admin/controllers/BusinessTypeController.php
@@ -1,34 +1,41 @@
 <?php
 
-class Admin_BusinessTypeController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
+class Admin_BusinessTypeController extends Zend_Controller_Action {
+	
+	public function init() {
 		/* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-        // action body
+	}
+	
+	public function indexAction() {
+		// action body
+	
+	}
+	
+	public function gridAction() {
+		$this->_helper->layout ()->disableLayout ();
+		$this->_helper->viewRenderer->setNoRender ( true );
+		$businessTypeMapper = new Admin_Model_Mapper_BusinessType();
 		
-    }
-    
-    public function gridAction()
-    {
-    	// action body
-    	
-    }
-
-    public function addAction()
-    {
-    	// action body
-    	
-    }
-    
-    public function editAction()
-    {
-    	// action body
-    	
-    }
+		$columns = array(
+				'business_type_id',
+				'name'
+		);
+		
+		$filter = " 1=1 ";
+		$gridData["sEcho"] = $this->_request->getParam("sEcho",1);
+		$gridData["iTotalRecords"] = $businessTypeMapper->countAll($filter);
+		$gridData["iTotalDisplayRecords"] = $businessTypeMapper->countAll($filter);
+		$gridData["aaData"] = $businessTypeMapper->getList($columns,null);
+		echo $this->_helper->json($gridData);
+	}
+	
+	public function addAction() {
+		// action body
+	
+	}
+	
+	public function editAction() {
+		// action body
+	
+	}
 }
--- application/modules/admin/models/mappers/BusinessType.php
+++ application/modules/admin/models/mappers/BusinessType.php
@@ -1,5 +1,20 @@
 <?php
-class Admin_Model_Mapper_BusinessType extends Standard_ModelMapper
-{
+class Admin_Model_Mapper_BusinessType extends Standard_ModelMapper {
 	protected $_dbTableClass = "Admin_Model_DbTable_BusinessType";
+	
+	public function getList(array $columns = array(), $where = null){
+		$businessTypeModels = $this->fetchAll($where);
+		$gridData = array();
+		foreach($businessTypeModels as $businessTypeModel) {
+			
+			$record = array();
+			foreach($columns as $column){
+				$record[] = $businessTypeModel->get($column);
+			}
+			$record[] = "";
+			$gridData[] = $record;
+		}
+		
+		return $gridData;
+	}
 }
